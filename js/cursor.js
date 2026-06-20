e1e52360efefab16264d65211e25d250e9c7d833
leongramminger <leongramminger@gmail.com>
2026-06-20 17:56:15 +0200

    second version

--- js/cursor.js
+++
@@ -1,8 +0,0 @@
-const heartCursor = document.getElementById("heart-cursor");
-
-document.addEventListener("mousemove", (e) => {
-  heartCursor.style.left = e.clientX + "px";
-  heartCursor.style.top  = e.clientY + "px";
-});
-document.addEventListener("mouseleave", () => { heartCursor.style.opacity = "0"; });
-document.addEventListener("mouseenter", () => { heartCursor.style.opacity = "1"; });
